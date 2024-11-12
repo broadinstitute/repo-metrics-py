e0ace71157459000175838ad1e3ae3cd33743d96
Kevin Lydon <klydon@broadinstitute.org>
2024-11-12 14:51:38 -0500

    Config stuff

--- src/repo_metrics/get/command.py
+++ src/repo_metrics/get/command.py
@@ -4,7 +4,7 @@ import logging
 import click
 
 from repo_metrics.metrics import DockerHubMetricsHelper, GitHubMetricsHelper
-from repo_metrics.output import Output, CsvOutput, JsonOutput, preprocess
+from repo_metrics.output import OutputConfig, Output, OutputType, CsvOutput, JsonOutput, preprocess
 
 LOGGER = logging.getLogger(__name__)
 
@@ -34,8 +34,8 @@ LOGGER = logging.getLogger(__name__)
 @click.option(
     "--output-format",
     "-of",
-    type=click.Choice(["json", "csv"]),
-    default="json",
+    type=click.Choice([o.value for o in OutputType]),
+    default=OutputType.JSON.value,
     help="The output format",
 )
 @click.option(
@@ -50,10 +50,24 @@ LOGGER = logging.getLogger(__name__)
     is_flag=True,
     help="Include a timestamp in the output",
 )
-def main(github_repo, dockerhub_repo, output, output_format, append, include_timestamp):
+@click.option(
+    "--config",
+    "-c",
+    type=str,
+    default="just_metrics",
+    help="Configuration to use. Use 'just_metrics' for just metrics that change over time, 'everything' for all available fields from the APIs, or a path to a json file with a custom configuration.",
+)
+def main(github_repo, dockerhub_repo, output, output_format, append, include_timestamp, config):
     """
     Get metrics for the specified repository
     """
+    if config == "just_metrics":
+        config = OutputConfig.just_metrics()
+    elif config == "everything":
+        config = OutputConfig.everything()
+    else:
+        config = OutputConfig.load_from_json_file(config)
+
     data_to_print = []
     data_to_print_labels = []
 
@@ -66,6 +80,9 @@ def main(github_repo, dockerhub_repo, output, output_format, append, include_tim
         owner, repo = github_repo.split("/")
         helper = GitHubMetricsHelper()
         github_data = helper.get_repo_info(owner, repo)
+        # Filter the fields if specified
+        if config.github_fields:
+            github_data = preprocess.filter(github_data, config.github_fields)
         data_to_print.append(github_data)
         data_to_print_labels.append("github")
 
@@ -73,6 +90,9 @@ def main(github_repo, dockerhub_repo, output, output_format, append, include_tim
         owner, repo = dockerhub_repo.split("/")
         helper = DockerHubMetricsHelper()
         dockerhub_data = helper.get_repo_info(owner, repo)
+        # Filter the fields if specified
+        if config.dockerhub_fields:
+            dockerhub_data = preprocess.filter(dockerhub_data, config.dockerhub_fields)
         data_to_print.append(dockerhub_data)
         data_to_print_labels.append("dockerhub")
     
--- src/repo_metrics/output/__init__.py
+++ src/repo_metrics/output/__init__.py
@@ -1,3 +1,4 @@
+from .config import OutputConfig
 from .csv_output import CsvOutput
 from .json_output import JsonOutput
 from .output_type import OutputType, Output
