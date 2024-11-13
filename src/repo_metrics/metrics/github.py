86e76d9ef61366d85b0937ccd10a03db7f43c23b
Kevin Lydon <klydon@broadinstitute.org>
2024-11-13 15:16:39 -0500

    Wrote some tests.  Need more still

--- src/repo_metrics/metrics/github.py
+++ src/repo_metrics/metrics/github.py
@@ -1,10 +1,20 @@
-import os
+import logging
 import requests
 
+from ..settings import Settings
+
+class GitHubException(Exception):
+    pass
+
+LOGGER = logging.getLogger(__name__)
 class GitHubMetricsHelper:
+    """
+    A helper class for getting metrics from the GitHub API
+    """
+
     def __init__(self):
         # Get the GitHub API token from the environment variable
-        token = os.getenv('GITHUB_TOKEN')
+        token = Settings().get_github_token()
         self.token = token
 
     def get_repo_info(self, owner: str, repo: str) -> dict:
@@ -19,6 +29,8 @@ class GitHubMetricsHelper:
         url = f'https://api.github.com/repos/{owner}/{repo}'
         headers = {'Authorization':f'token {self.token}'}
         response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            raise GitHubException(f"Failed to get info for {owner}/{repo}")
         data = response.json()
 
         # Get the download count for the repository
@@ -46,15 +58,18 @@ class GitHubMetricsHelper:
             # Set the per_page parameter to 100 to get the maximum number of releases per page
             params = {'page': page, 'per_page': 100}
             response = requests.get(url, headers=headers, params=params)
+            if response.status_code != 200:
+                raise GitHubException(f"Failed to get info for {owner}/{repo}")
             releases = response.json()
+
+            # If there are no more releases, break out of the loop
+            if not releases:
+                break
             
             for release in releases:
                 for asset in release['assets']:
                     download_count += asset['download_count']
             
-            # If there are no more releases, break out of the loop
-            if not releases:
-                break
 
             page += 1
         
